a7a3c6481f163c36e36e1ac74b77a7dc9777f163
Vizarri <82329177+Vizarri@users.noreply.github.com>
2021-07-21 23:36:53 +0400

    Add files via upload

---
+++ HUD.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+// Класс отвечающий за вывод на экран количества столкновений
+
+public class HUD : MonoBehaviour
+{
+    [SerializeField]
+    Text textOutput;
+    int collision=0;
+
+    public void AddBounce()
+    {
+        collision += 1;
+        textOutput.text = collision.ToString();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textOutput.text = collision.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
